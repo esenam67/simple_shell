0a784897a3ea6bd318677873d8d1ff202e4f889a
esenam67 <moneyesenam@gmail.com>
2023-12-04 11:02:50 +0000

    my commit

---
+++ ProDisplay.c
@@ -0,0 +1,18 @@
+#include "shellsh.h"
+
+/**
+ * main - shell like function
+ * Description: Printing a prompt read and return an output
+ * Return: 0
+ */
+
+int main(void)
+{
+char *ThePro = "$ ";
+while (1)
+{
+write(STDOUT_FILENO, ThePro, 2);
+thegetline();
+}
+return (0);
+}
